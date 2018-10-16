ebe271764ba4e17b46d6f047e3b48ecd1ac93f0c
nhtuan231096 <nhtuan2313096@gmail.com>
2018-10-16 17:49:06 +0700

    Tuan

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::group(['prefix'=>'admin','namespace'=>'Admin','middleware'=>'auth'],function(){
+	Route::get('','AdminController@index')->name('HomeAdmin');
+});
+// Route login admin
+Route::get('admin/login.html','Admin\AdminController@login')->name('login');
+Route::post('admin/login.html','Admin\AdminController@post_login')->name('login');
+
+// Route logout admin
+Route::get('admin/logout','Admin\AdminController@logout')->name('loginAdmin');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
